7331a9724db122d10814eddbb78b627922dd3130
João Paulo Prata <55767830+JoaoPauloPrata@users.noreply.github.com>
2022-01-11 21:09:11 -0300

    Update OneAppl.java

--- pubsub/src/appl/OneAppl.java
+++ pubsub/src/appl/OneAppl.java
@@ -20,11 +20,11 @@ public class OneAppl {
        
         joubert.subscribe("10.128.0.2", 8080);
         debora.subscribe("10.128.0.2", 8080);
-        jonata.subscribe("10.128.0.5", 8081);
+        jonata.subscribe("10.128.0.2", 8081);
         
         Thread accessOne = new ThreadWrapper(joubert, "access Joubert- var x", "10.128.0.2", 8080);
         Thread accessTwo = new ThreadWrapper(debora, "access Debora- var x", "10.128.0.2", 8080);
-        Thread accessThree = new ThreadWrapper(jonata, "access Jonata- var x", "10.128.0.5", 8081);
+        Thread accessThree = new ThreadWrapper(jonata, "access Jonata- var x", "10.128.0.2", 8081);
                
         accessOne.start();
         accessTwo.start();
