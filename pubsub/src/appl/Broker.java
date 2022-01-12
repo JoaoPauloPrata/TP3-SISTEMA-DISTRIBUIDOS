e1cfe0b3a8a093796e3eef0a0606f03eac133af2
João Paulo Prata <55767830+JoaoPauloPrata@users.noreply.github.com>
2022-01-11 21:16:42 -0300

    Update Broker.java

--- pubsub/src/appl/Broker.java
+++ pubsub/src/appl/Broker.java
@@ -15,9 +15,9 @@ public class Broker {
         System.out.print("Is the broker primary? (Y/N): ");
         String respYN = reader.next();
 
-        //System.out.print("Enter the secondary Broker address: ");
-        //String secondAddress = reader.next();
-        String secondAddress = "10.128.0.5";
+        System.out.print("Enter the secondary Broker address: ");
+        String secondAddress = reader.next();
+        //String secondAddress = "10.128.0.5";
 
         System.out.print("Enter the secondary Broker port number: ");
         int secondPort = reader.nextInt();
