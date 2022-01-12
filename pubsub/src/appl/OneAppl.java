25808da26c70ee57b2637b747311b6ebfffef15e
João Paulo Prata <55767830+JoaoPauloPrata@users.noreply.github.com>
2022-01-11 21:11:36 -0300

    Update OneAppl.java

--- pubsub/src/appl/OneAppl.java
+++ pubsub/src/appl/OneAppl.java
@@ -72,7 +72,7 @@ public class OneAppl {
         
         joubert.unsubscribe("10.128.0.2", 8080);
         debora.unsubscribe("10.128.0.2", 8080);
-        jonata.unsubscribe("10.128.0.5", 8080);
+        jonata.unsubscribe("10.128.0.2", 8080);
 
         joubert.stopPubSubClient();
         debora.stopPubSubClient();
